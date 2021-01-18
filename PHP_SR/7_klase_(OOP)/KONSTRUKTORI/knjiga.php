195f145064c39fcaf4de21f296d64c1f83da692a
KosticDusan4D <kosticdusan4d@gmail.com>
2021-01-18 23:18:25 +0100

    skolski rad 18.01

---
+++ PHP_SR/7_klase_(OOP)/KONSTRUKTORI/knjiga.php
@@ -0,0 +1,177 @@
+<?php
+
+class Knjiga{
+    private $naslov;
+    private $autor;
+    private $godIzdanja;
+    private $brojStrana;
+    private $cena;
+
+    // Konstruktor
+    public function __construct($n, $a, $g, $b, $c)
+    {
+       $this->setNaslov($n);
+       $this->setAutor($a);
+       $this->setGodIzdanja($g);
+       $this->setBrojStrana($b);
+       $this->setCena($c);
+    }
+
+    // Seteri
+    public function setNaslov($n){
+       $this->naslov = $n;
+    }
+    public function setAutor($a){
+       $this->autor = $a;
+    }
+    public function setGodIzdanja($g){
+       $this->godIzdanja = $g;
+    }
+    public function setBrojStrana($b){
+       $this->brojStrana = $b;
+    }
+    public function setCena($c){
+       $this->cena = $c;
+    }
+
+    // Geteri
+    public function getNaslov(){
+       return $this->naslov;
+    }
+    public function getAutor(){
+       return $this->autor;
+    }
+    public function getGodIzdanja(){
+       return $this->godIzdanja;
+    }
+    public function getBrojStrana(){
+       return $this->brojStrana;
+    }
+    public function getCena(){
+       return $this->cena;
+    }
+
+    // Metode
+    function stampaj(){
+       echo 
+       "<ul>
+       <li>Naslov: '{$this->getNaslov()}'</li>
+       <li>Autor: {$this->getAutor()}</li>
+       <li>Izdata: {$this->getGodIzdanja()}. godine</li>
+       <li>Broj strana: {$this->getBrojStrana()}</li>
+       <li>Cena: {$this->getCena()} RSD</li>
+       </ul>";
+    }
+ 
+    function obimna(){
+       $brStr = $this->getBrojStrana();
+       if($brStr > 600){
+          return true;
+       }
+       else{
+          return false;
+       }
+    }
+ 
+    function skupa(){
+       $cena = $this->getCena();
+       if($cena > 8000){
+          return true;
+       }
+       else{
+          return false;
+       }
+    }
+ 
+    function dugackoIme(){
+       $imeAutora = $this->getAutor();
+       $duzinaImena = strlen($imeAutora);
+       if($duzinaImena > 18){
+          return true;
+       }
+       else{
+          return false;
+       }
+    }
+}
+
+$knjiga1 = new Knjiga("Naslov 1", "Autor 1", 1900, 100, 8500);
+$knjiga2 = new Knjiga("Naslov 2", "Autor 1", 1905, 650, 9500);
+$knjiga3 = new Knjiga("Naslov 3", "AutorAutorAutorAutor 2", 2009, 300, 800);
+$knjiga4 = new Knjiga("Naslov 4", "AutorAutorAutorAutor 2", 2015, 350, 1000);
+$knjiga5 = new Knjiga("Naslov 5", "Autor 3", 2020, 2700, 9500);
+
+$knjige = array($knjiga1, $knjiga2, $knjiga3, $knjiga4, $knjiga5);
+
+//ISPISATI SVE AUTORE KOJIMA JE IME DUGACKO
+echo "<p>ISPISATI SVE AUTORE KOJIMA JE IME DUGACKO</p>";
+foreach($knjige as $k){
+    if($k->dugackoIme()){
+        echo "<p>{$k->getAutor()}</p>";
+    }
+}
+
+echo "<hr>";
+
+//Ispisati sve one knjige koje su istovremeno i skupe i obimne
+echo "<p>Ispisati sve one knjige koje su istovremeno i skupe i obimne</p>";
+foreach($knjige as $k){
+    if($k->skupa() && $k->obimna()){
+        $k->stampaj();
+    }
+}
+
+echo "<hr>";
+
+//Napraviti funkciju koliko ukupno kostaju sve knjige u nizu knjiga
+echo "<p>Napraviti funkciju koliko ukupno kostaju sve knjige u nizu knjiga</p>";
+
+function ukupnaCena($knjige){
+    $ukupno = 0;
+    //SA FOR PETLJOM
+    // for($i = 0; $i<count($knjige); $i++){
+    //     $k = $knjige[$i];
+    //     $ukupno += $k->getCena();
+    // }
+    //SA FOREACH PETLJOM
+    foreach($knjige as $k){
+        $ukupno += $k->getCena();
+    } 
+    return $ukupno;
+}
+
+echo "Ukupna cena knjiga je " . ukupnaCena($knjige);
+
+echo "<hr>";
+
+//Napraviti funkciju: Koliko ja prosecna cena jedne knjige?
+echo "<p>Koliko ja prosecna cena jedne knjige?</p>";
+
+function prosecnaCena($niz){
+    $ukupnacena = ukupnaCena($niz);
+    $br = count($niz);
+    return $ukupnacena/$br;
+}
+
+echo "Prosecna cena knjige je " . prosecnaCena($knjige);
+
+echo "<hr>";
+
+//Napraviti funkciju: Kolika je prosečna cena stranice knjige
+echo "<p>Kolika je prosečna cena stranice knjige?</p>";
+
+function prosecnaStranica($knjige){
+    $ukupnaCena = ukupnaCena($knjige);
+    $ukupnoStranica = 0;
+    foreach($knjige as $k){
+        $ukupnoStranica += $k->getBrojStrana();
+    }
+    return $ukupnaCena/$ukupnoStranica;
+}
+
+echo "Prosecna cena stranice knjige je " . prosecnaStranica($knjige);
+
+
+
+
+?>
