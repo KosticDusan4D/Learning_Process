a1153299e7c1a1dbeafd7596b0b09a521c1e6c33
KosticDusan4D <kosticdusan.4d@gmail.com>
2021-04-16 00:38:57 +0200

    chapter10

---
+++ JS_NINJA/chapter_9/Lesson_71_Filter_Method/sandbox.js
@@ -0,0 +1,24 @@
+const scores = [10, 30, 15, 25, 50, 40, 5];
+
+// const filteredScores = scores.filter((score) => {
+//     return score > 20;
+// });
+
+// console.log(scores);
+// console.log(filteredScores);
+
+const users = [
+    {name: 'shaun', premium: true},
+    {name: 'youshi', premium: false},
+    {name: 'mario', premium: false},
+    {name: 'chun-li', premium: true}
+];
+
+// const premiumUsers = users.filter(user => {
+//     return user.premium;
+// });
+
+//Skracena verzija
+const premiumUsers = users.filter(user => user.premium);
+
+console.log(premiumUsers);
